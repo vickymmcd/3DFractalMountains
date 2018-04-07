0a649c40f83e302199fe9e03051997be692dd3d4
willythor <willem.thorbecke@students.olin.edu>
2018-04-07 15:39:10 -0400

    run this code with this command: g++tg.cpp terrain.cpp tga.cpp -lglut -lGL -lGLU -o tg

---
+++ terrain0/terrain.h
@@ -0,0 +1,11 @@
+#define TERRAIN_ERROR_LOADING_IMAGE		-4
+#define TERRAIN_ERROR_MEMORY_PROBLEM	-3
+#define	TERRAIN_ERROR_NOT_SAVED			-2
+#define TERRAIN_ERROR_NOT_INITIALISED	-1
+#define TERRAIN_OK						 0
+
+int terrainLoadFromImage(char *filename, int normals);
+int terrainCreateDL(float xOffset, float yOffset, float zOffset);
+void terrainDestroy();
+int terrainScale(float min,float max);
+float terrainGetHeight(int x, int z);
